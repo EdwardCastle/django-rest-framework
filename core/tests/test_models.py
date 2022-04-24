7073306f8fe898958c582b0eef36be8fbe198da6
edwardcastle <sir.edwardcastle@gmail.com>
2022-04-24 12:24:09 -0400

    initial commit

---
+++ core/tests/test_models.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTest(TestCase):
+
+    def create_user_with_email(self):
+        """ Test for creating a new user email correctly """
+
+        email = 'edward@castle.com'
+        password = 'test123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
