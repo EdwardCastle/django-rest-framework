ad89d4b510a4e1375849341ce4c19d3604f177dc
edwardcastle <sir.edwardcastle@gmail.com>
2022-04-26 22:21:36 -0400

    feat: Created Ingredient serializer, viewsets and tests

---
+++ recipe/tests/test_ingredients_api.py
@@ -0,0 +1,81 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+from core.models import Ingredient
+from recipe.serializers import IngredientSerializer
+
+INGREDIENT_URL = reverse('recipe:ingredient-list')
+
+
+class PublicIngredientApiTests(TestCase):
+    """ Test API Ingredient public access """
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_login_required(self):
+        """ Test that login is necessary to access to this endpoint """
+        request = self.client.get(INGREDIENT_URL)
+        self.assertEqual(request.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateIngredientApiTests(TestCase):
+    """ Test API Ingredient private access """
+
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            'edward@castle.com',
+            'test123'
+        )
+        self.client.force_authenticate(self.user)
+
+    def test_retrieve_ingredient_list(self):
+        """ Test retrieve list of ingredients """
+        Ingredient.objects.create(user=self.user, name='Tomato')
+        Ingredient.objects.create(user=self.user, name='Sugar')
+
+        request = self.client.get(INGREDIENT_URL)
+
+        ingredients = Ingredient.objects.all().order_by('-name')
+        serializer = IngredientSerializer(ingredients, many=True)
+        self.assertEqual(request.status_code, status.HTTP_200_OK)
+        self.assertEqual(request.data, serializer.data)
+
+    def test_ingredients_limited_to_user(self):
+        """ Test return ingredients just authenticated by the user """
+        user2 = get_user_model().objects.create_user(
+            'testuser',
+            'testpass'
+        )
+        Ingredient.objects.create(user=user2, name='Vinegar')
+        ingredient = Ingredient.objects.create(user=self.user, name='gallic')
+
+        request = self.client.get(INGREDIENT_URL)
+        self.assertEqual(request.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(request.data), 1)
+        self.assertEqual(request.data[0]['name'], ingredient.name)
+
+    def test_create_ingredient_succesfully(self):
+        """ Test create new ingredient """
+
+        payload = {'name': 'chocolate'}
+
+        self.client.post(INGREDIENT_URL, payload)
+
+        exists = Ingredient.objects.filter(
+            user=self.user,
+            name=payload['name']
+        ).exists()
+
+        self.assertTrue(exists)
+
+    def test_create_invalid_ingredient(self):
+        """ Test create invalid ingredient """
+        payload = {'name': ''}
+
+        request = self.client.post(INGREDIENT_URL, payload)
+
+        self.assertEqual(request.status_code, status.HTTP_400_BAD_REQUEST)
--- recipe/views.py
+++ recipe/views.py
@@ -1,8 +1,9 @@
 from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
-from core.models import Tag
+from core.models import Tag, Ingredient
 from recipe import serializers
+from recipe.serializers import IngredientSerializer
 
 
 class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
@@ -13,9 +14,26 @@ class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateMo
     serializer_class = serializers.TagSerializer
 
     def get_queryset(self):
-        """ Return objects for the authenticated user """
+        """ Return Tags objects for the authenticated user """
         return self.queryset.filter(user=self.request.user).order_by('-name')
 
     def perform_create(self, serializer):
         """ Create new Tag """
+        serializer.save(user=self.request.user)
+
+
+class IngredientViewset(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
+    """ Ingredient handler in the database """
+
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    queryset = Ingredient.objects.all()
+    serializer_class = serializers.IngredientSerializer
+
+    def get_queryset(self):
+        """ Return Ingredients objects for the authenticated user """
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        """ Create new Ingredient """
         serializer.save(user=self.request.user)
